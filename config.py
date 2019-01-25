0e232dcda249626df51ce6db775d44d0e5021c33
OrestSkorokhod <orestskorohod@gmail.com>
2019-01-25 18:40:06 +0200

    db

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Configuration(object):
 	DEBUG = True
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+mysqlconnector://root:1@localhost/blog_flask'
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	SECRET_KEY = '123'
 
